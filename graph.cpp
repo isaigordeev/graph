d188af190d82581210cb7aa9e04acb14e9573cb6
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2024-01-28 15:28:44 +0100

    graph class check vertices

--- graph.cpp
+++ graph.cpp
@@ -7,3 +7,47 @@
 #include <fstream>
 
 
+#include <iostream>
+#include <vector>
+
+class Graph {
+private:
+    int vertices;
+    std::vector<std::vector<int>> adjacencyList;
+
+public:
+    Graph(int V) : vertices(V), adjacencyList(V) {}
+
+    void addEdge(int u, int v) {
+        adjacencyList[u].push_back(v);
+        adjacencyList[v].push_back(u);
+    }
+
+    void dfs(int vertex, std::vector<bool>& visited) {
+        visited[vertex] = true;
+        std::cout << vertex << " ";
+
+        for (int adjacentVertex : adjacencyList[vertex]) {
+            if (!visited[adjacentVertex]) {
+                dfs(adjacentVertex, visited);
+            }
+        }
+    }
+
+    void dfsTraversal() {
+        std::vector<bool> visited(vertices, false);
+
+        std::cout << "Depth-First Traversal:" << std::endl;
+        for (int i = 0; i < vertices; ++i) {
+            if (!visited[i]) {
+                dfs(i, visited);
+            }
+        }
+
+        std::cout << std::endl;
+    }
+};
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "utils.cpp"
 #include <unistd.h>
 #include <filesystem>
+#include "graph.cpp"
 
 int main() {
     std::string folderName = "INF421_PI_tests";
@@ -33,5 +34,15 @@ int main() {
 
     inputFile.close();
 
+    Graph g(5);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+
+    g.dfsTraversal();
+
     return 0;
+
 }
